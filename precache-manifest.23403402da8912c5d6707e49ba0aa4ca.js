670433a148c8c4cf85e8b78b13964995f872a1a3
Natalia Malcova <nataliamalkova@yahoo.com>
2019-10-02 00:57:11 -0400

    Updates

---
+++ precache-manifest.23403402da8912c5d6707e49ba0aa4ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f10a6cc51ed0f5fe300c92207d4d0c9",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f5863f86872e966afa60",
+    "url": "/Clicky-Game/static/css/main.4bb68808.chunk.css"
+  },
+  {
+    "revision": "23a5aadf743426d69a2f",
+    "url": "/Clicky-Game/static/js/2.54d6ae92.chunk.js"
+  },
+  {
+    "revision": "f5863f86872e966afa60",
+    "url": "/Clicky-Game/static/js/main.7a1d1dbd.chunk.js"
+  },
+  {
+    "revision": "ce9dd986494513bf7ced",
+    "url": "/Clicky-Game/static/js/runtime-main.b33e6778.js"
+  }
+]);
